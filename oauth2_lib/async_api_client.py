a1195110c21b5d1d47f0fd903046073f67a1f4f5
Peter Boers <peter.boers@surfnet.nl>
2021-02-10 11:32:27 +0100

    Added Otel manual tracing for URLLib3

--- oauth2_lib/async_api_client.py
+++ oauth2_lib/async_api_client.py
@@ -10,14 +10,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import contextlib
 from asyncio import new_event_loop
 from http import HTTPStatus
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Generator, Optional
 
 import structlog
+import urllib3
 from authlib.integrations.base_client import BaseOAuth, RemoteApp
+from opentelemetry import context
+from opentelemetry.instrumentation.utils import http_status_to_status_code
+from opentelemetry.instrumentation.version import __version__
+from opentelemetry.propagators import inject
+from opentelemetry.trace import Span, SpanKind, get_tracer
+from opentelemetry.trace.status import Status
 
 logger = structlog.get_logger(__name__)
+
+_SUPPRESS_HTTP_INSTRUMENTATION_KEY = "suppress_http_instrumentation"
 
 
 def is_api_exception(ex: Exception) -> bool:
@@ -35,6 +45,27 @@ def is_api_exception(ex: Exception) -> bool:
 
     """
     return ex.__class__.__name__ == "ApiException"
+
+
+def _apply_response(span: Span, response: urllib3.response.HTTPResponse) -> None:
+    if not span.is_recording():
+        return
+    span.set_attribute("http.status_code", response.status)
+    span.set_attribute("http.status_text", response.reason)
+    span.set_status(Status(http_status_to_status_code(response.status)))
+
+
+@contextlib.contextmanager
+def _suppress_further_instrumentation() -> Generator:
+    token = context.attach(context.set_value(_SUPPRESS_HTTP_INSTRUMENTATION_KEY, True))
+    try:
+        yield
+    finally:
+        context.detach(token)
+
+
+def _is_instrumentation_suppressed() -> bool:
+    return bool(context.get_value("suppress_instrumentation") or context.get_value(_SUPPRESS_HTTP_INSTRUMENTATION_KEY))
 
 
 class AsyncAuthMixin:
@@ -68,15 +99,17 @@ class AsyncAuthMixin:
         oauth_client: BaseOAuth,
         oauth_client_name: str,
         oauth_active: bool,
+        tracing_enabled: bool,
         *args: Any,
         **kwargs: Any,
     ):
         super().__init__(*args, **kwargs)  # type:ignore
         self._oauth_client: RemoteApp = getattr(oauth_client, oauth_client_name)
         self._oauth_active = oauth_active
+        self._tracing_enabled = tracing_enabled
         self._token = None
 
-    def add_client_creds_token_header(self, headers: Optional[Dict[str, Any]]) -> None:
+    def add_client_creds_token_header(self, headers: Dict[str, Any]) -> None:
         """Add header with credentials to an existing set of headers.
 
         This function assumes the `access_token` has been set in the application configuration
@@ -92,7 +125,6 @@ class AsyncAuthMixin:
         if not self._token and self._oauth_active:
             loop = new_event_loop()
             loop.run_until_complete(self.refresh_client_creds_token())
-        headers = {} if headers is None else headers
         if self._token:
             access_token = self._token
             headers["Authorization"] = f"bearer {access_token['access_token']}"
@@ -126,22 +158,46 @@ class AsyncAuthMixin:
         _preload_content=True,
         _request_timeout=None,
     ):
+        headers = {} if headers is None else headers
 
-        try:
-            self.add_client_creds_token_header(headers)
-            return super().request(  # type:ignore
-                method, url, query_params, headers, post_params, body, _preload_content, _request_timeout
-            )
-        except Exception as ex:
-            if is_api_exception(ex) and ex.status in (HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN):  # type:ignore
-                logger.warning("Access Denied. Token expired? Retrying.", api_exception=str(ex))
-                loop = new_event_loop()
-                loop.run_until_complete(self.refresh_client_creds_token(force=True))
+        span_attributes = {
+            "http.method": method,
+            "http.url": url,
+        }
+
+        with get_tracer(__name__, __version__).start_as_current_span(
+            f"External Api Call {self.__class__.__name__}", kind=SpanKind.CLIENT, attributes=span_attributes
+        ) as span:
+            try:
                 self.add_client_creds_token_header(headers)
-                return super().request(  # type:ignore
-                    method, url, query_params, headers, post_params, body, _preload_content, _request_timeout
-                )
 
-            else:
-                logger.exception("Could not call API.", client=self.__class__.__name__)
-                raise
+                if self._tracing_enabled and not _is_instrumentation_suppressed():
+                    inject(type(headers).__setitem__, headers)
+
+                with _suppress_further_instrumentation():
+                    response = super().request(  # type:ignore
+                        method, url, query_params, headers, post_params, body, _preload_content, _request_timeout
+                    )
+                _apply_response(span, response)
+                return response
+            except Exception as ex:
+                if is_api_exception(ex) and ex.status in (HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN):  # type:ignore
+                    logger.warning("Access Denied. Token expired? Retrying.", api_exception=str(ex))
+                    loop = new_event_loop()
+                    loop.run_until_complete(self.refresh_client_creds_token(force=True))
+                    self.add_client_creds_token_header(headers)
+
+                    if self._tracing_enabled and not _is_instrumentation_suppressed():
+                        inject(type(headers).__setitem__, headers)
+
+                    with _suppress_further_instrumentation():
+                        response = super().request(  # type:ignore
+                            method, url, query_params, headers, post_params, body, _preload_content, _request_timeout
+                        )
+                    _apply_response(span, response)
+                    return response
+
+                else:
+                    logger.exception("Could not call API.", client=self.__class__.__name__)
+                    _apply_response(span, ex)
+                    raise
