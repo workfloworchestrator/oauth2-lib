2189af1fa1b5d1a46d6f8a453a7d07c95c1fecdd
Mark90 <mark_moes@live.nl>
2023-04-25 17:54:06 +0200

    Testcases for AsyncAuthMixin with verification of OTEL traces

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from unittest import mock
 
 import pytest
 from httpx import AsyncClient, Response
+from urllib3_mock import Responses
 
 
 @pytest.fixture(scope="session")
@@ -23,3 +24,72 @@ def make_mock_async_client():
         return mock_async_client
 
     return _make_mock_async_client
+
+
+@pytest.fixture(autouse=False)
+def responses():
+    responses_mock = Responses("requests.packages.urllib3")
+
+    def _find_request(call):
+        if not (mock_url := responses_mock._find_match(call.request)):
+            raise Exception(f"Call not mocked: {call.request}")
+        return mock_url
+
+    def _to_tuple(url_mock):
+        return (url_mock["url"], url_mock["method"], url_mock["match_querystring"])
+
+    with responses_mock:
+        yield responses_mock
+
+        mocked_urls = map(_to_tuple, responses_mock._urls)
+        used_urls = map(_to_tuple, map(_find_request, responses_mock.calls))
+        if not_used := set(mocked_urls) - set(used_urls):
+            pytest.fail(f"Found unused responses mocks: {not_used}", pytrace=False)
+
+
+@pytest.fixture(scope="session")
+def tracing():
+    from opentelemetry import trace
+    from opentelemetry.instrumentation.urllib3 import URLLib3Instrumentor
+    from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import BatchSpanProcessor
+    from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+
+    resource = Resource(attributes={SERVICE_NAME: "oauth2-lib-unittests"})
+
+    provider = TracerProvider(resource=resource)
+
+    # Uncomment to print processed spans to stdout
+
+    # from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+    # console_exporter = ConsoleSpanExporter()
+    # console_processor = BatchSpanProcessor(console_exporter)
+    # provider.add_span_processor(console_processor)
+
+    in_memory_exporter = InMemorySpanExporter()
+    in_memory_processor = BatchSpanProcessor(in_memory_exporter)
+    provider.add_span_processor(in_memory_processor)
+
+    trace.set_tracer_provider(provider)
+
+    def strip_query_params(url: str) -> str:
+        return url.split("?")[0]
+
+    # Remove all query params from the URL attribute on the span
+    URLLib3Instrumentor().instrument(url_filter=strip_query_params)
+
+    # Return the in-memory-processor so we can inspect the generated spans
+    yield in_memory_processor
+
+    try:
+        provider.shutdown()
+    except Exception as exc:
+        print("Error shutting down TracerProvider, ignoring:", str(exc))  # noqa: T201
+
+
+@pytest.fixture(autouse=True)
+def clear_spans(tracing):
+    # The tracerprovider runs the entire test-session so we have to clean up the spans
+    # after each test
+    tracing.span_exporter.clear()
---
+++ tests/test_async_api_client.py
@@ -0,0 +1,223 @@
+import json
+from http import HTTPStatus
+from unittest import mock
+
+import pytest
+import urllib3
+from opentelemetry.sdk.trace import ReadableSpan
+from urllib3_mock import Responses
+
+from oauth2_lib.async_api_client import AsyncAuthMixin
+
+EXPIRED_TOKEN = "expired token"  # noqa: S105
+
+VALID_TOKEN = "valid token"  # noqa: S105
+
+BASE_URL = "http://my-api"
+
+
+class ApiException(Exception):
+    # Copy of OpenAPI's ApiException class
+
+    def __init__(self, status=None, reason=None, http_resp=None):
+        if http_resp:
+            self.status = http_resp.status
+            self.reason = http_resp.reason
+            self.body = http_resp.data
+            self.headers = http_resp.headers
+        else:
+            self.status = status
+            self.reason = reason
+            self.body = None
+            self.headers = None
+
+    def __str__(self):
+        error_message = "({})\n" "Reason: {}\n".format(self.status, self.reason)
+        if self.headers:
+            error_message += f"HTTP response headers: {self.headers}\n"
+
+        if self.body:
+            error_message += f"HTTP response body: {self.body}\n"
+
+        return error_message
+
+
+def make_api_client(url=BASE_URL, token=VALID_TOKEN):
+    """Create an api client similar to these.
+
+    > ims_api_client = ImsApiClient(
+    >     oauth_client=oauth_client_credentials,
+    >     oauth_client_name="<hidden>",
+    >     oauth_active=oauth2_settings.OAUTH2_ACTIVE_REQUESTS,
+    >     host=external_service_settings.IMS_URI,
+    > )
+    """
+
+    class FakeApiClient:
+        # Simplified OpenApi Client
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def request(self, method, url, query_params, headers, *args):
+            http = urllib3.PoolManager()
+            response = http.request(method, url, headers=headers)
+            if not 200 <= response.status <= 299:
+                raise ApiException(http_resp=response)
+            return response
+
+    class FakeFinalApiClient(AsyncAuthMixin, FakeApiClient):
+        # Simplified client implementation of the OpenApi Client
+        def __init__(self, host, *args, **kwargs):
+            config = mock.MagicMock()
+            config.host = host
+            config.access_token = None
+            config.access_token = None
+
+            async def mock_fetch_access_token():
+                return {"access_token": VALID_TOKEN}
+
+            base_oauth_client = mock.MagicMock()
+            oauth_client = mock.MagicMock()
+            oauth_client.fetch_access_token = mock_fetch_access_token
+            base_oauth_client.actualclient = oauth_client
+            super().__init__(
+                oauth_client=base_oauth_client,
+                oauth_client_name="actualclient",
+                oauth_active=True,
+                tracing_enabled=True,
+                configuration=config,
+            )
+
+            self._token = {"access_token": token}
+
+    return FakeFinalApiClient(url)
+
+
+class ApiMock:
+    def __init__(self, responses: Responses):
+        self.responses = responses
+
+    def get_endpoint(self):
+        def verify_token(request):
+            if request.headers["Authorization"] == f"bearer {VALID_TOKEN}":
+                return HTTPStatus.OK, {}, json.dumps({"data": "hello"})
+
+            if request.headers["Authorization"] == f"bearer {EXPIRED_TOKEN}":
+                return HTTPStatus.UNAUTHORIZED, {}, json.dumps({"error": "unauthorized"})
+
+            unrecognized_token = request.headers["Authorization"]
+            raise AssertionError(f"Invalid access token {unrecognized_token}")
+
+        self.responses.add_callback("GET", "/app/endpoint", callback=verify_token, content_type="application/json")
+
+    def get_endpoint_500(self):
+        self.responses.add(
+            "GET",
+            "/app/endpoint",
+            body=json.dumps({"error": "unknown"}),
+            status=HTTPStatus.INTERNAL_SERVER_ERROR,
+            content_type="application/json",
+        )
+
+    def get_endpoint_404(self):
+        self.responses.add(
+            "GET",
+            "/app/endpoint",
+            body=json.dumps({"error": "not found"}),
+            status=HTTPStatus.NOT_FOUND,
+            content_type="application/json",
+        )
+
+
+def test_asyncauthmixin_request_200(tracing, responses):
+    """Test that making a request with valid token works."""
+
+    # given
+    client = make_api_client()
+
+    apimock = ApiMock(responses)
+    apimock.get_endpoint()
+
+    # when
+    res = client.request("GET", f"{BASE_URL}/app/endpoint")
+
+    # then
+    assert res.status == HTTPStatus.OK
+    assert len(responses.calls) == 1
+
+    tracing.force_flush()  # Force finish the spans
+
+    spans = tracing.span_exporter._finished_spans  # type: ignore
+    assert len(spans) == 1
+    span: ReadableSpan = spans[0]
+    assert span.attributes["http.status_code"] == HTTPStatus.OK
+    assert span.attributes["http.url"] == f"{BASE_URL}/app/endpoint"
+
+
+def test_asyncauthmixin_request_401_then_200(tracing, responses):
+    """Test that a request with expired token is retried with a valid token."""
+    # given
+    client = make_api_client(token=EXPIRED_TOKEN)
+    apimock = ApiMock(responses)
+    apimock.get_endpoint()
+
+    # when
+    res = client.request("GET", f"{BASE_URL}/app/endpoint")
+
+    # then
+    assert res.status == HTTPStatus.OK
+    assert len(responses.calls) == 2
+
+    tracing.force_flush()  # Force finish the spans
+    spans = tracing.span_exporter._finished_spans  # type: ignore
+
+    assert len(spans) == 1
+    span: ReadableSpan = spans[0]
+    assert span.attributes["http.status_code"] == HTTPStatus.OK
+    assert span.attributes["http.url"] == f"{BASE_URL}/app/endpoint"
+
+
+def test_asyncauthmixin_request_500(tracing, responses):
+    # given
+    client = make_api_client(token=EXPIRED_TOKEN)
+    apimock = ApiMock(responses)
+    apimock.get_endpoint_500()
+
+    # when
+    with pytest.raises(ApiException) as exc:
+        client.request("GET", f"{BASE_URL}/app/endpoint")
+
+    # then
+    assert exc.value.status == HTTPStatus.INTERNAL_SERVER_ERROR
+    assert len(responses.calls) == 1
+
+    tracing.force_flush()  # Force finish the spans
+    spans = tracing.span_exporter._finished_spans  # type: ignore
+
+    assert len(spans) == 1
+    span: ReadableSpan = spans[0]
+    assert span.attributes["http.status_code"] == HTTPStatus.INTERNAL_SERVER_ERROR
+    assert span.attributes["http.url"] == f"{BASE_URL}/app/endpoint"
+
+
+def test_asyncauthmixin_request_404(tracing, responses):
+    # given
+    client = make_api_client(token=EXPIRED_TOKEN)
+    apimock = ApiMock(responses)
+    apimock.get_endpoint_404()
+
+    # when
+    with pytest.raises(ApiException) as exc:
+        client.request("GET", f"{BASE_URL}/app/endpoint")
+
+    # then
+    assert exc.value.status == HTTPStatus.NOT_FOUND
+    assert len(responses.calls) == 1
+
+    tracing.force_flush()  # Force finish the spans
+    spans = tracing.span_exporter._finished_spans  # type: ignore
+
+    assert len(spans) == 1
+    span: ReadableSpan = spans[0]
+    assert span.attributes["http.status_code"] == HTTPStatus.NOT_FOUND
+    assert span.attributes["http.url"] == f"{BASE_URL}/app/endpoint"
