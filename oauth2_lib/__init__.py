ec26716798cf17a7536623633474d14d6d3a0e85
Mark Moes <mark_moes@live.nl>
2024-01-19 11:24:06 +0100

    Add Python 3.12 support (#60)
    * Add python 3.12 support to test matrix and package metadata
    
    Loosen httpx constraint
    
    * Bump version 1.5.0
    
    * Happy new year + mypy

--- oauth2_lib/__init__.py
+++ oauth2_lib/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2023 SURF.
+# Copyright 2019-2024 SURF.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,4 +13,4 @@
 
 """This is the SURF Oauth2 module that interfaces with the oauth2 setup."""
 
-__version__ = "1.4.2"
+__version__ = "1.5.0"
